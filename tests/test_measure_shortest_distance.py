08c8f28ae330e3edcb789830fd18c1290251b9e5
Flurin Sturzenegger <st.flurin@gmail.com>
2025-11-28 14:08:59 +0100

    add new tests for measurements

--- tests/test_measure_shortest_distance.py
+++ tests/test_measure_shortest_distance.py
@@ -1,15 +1,12 @@
 from zmb_fractal_tasks.from_fractal_tasks_core.channels import ChannelInputModel
-from zmb_fractal_tasks.measure_features import measure_features
+from zmb_fractal_tasks.measure_shortest_distance import measure_shortest_distance
 
-def test_measure_features(zarr_MIP_path):
-    measure_features(
+def test_measure_shortest_distance(zarr_MIP_path):
+    measure_shortest_distance(
         zarr_url=str(zarr_MIP_path / "B" / "03" / "0"),
         output_table_name="nuclei_features",
         label_name="nuclei",
-        channels_to_include=[ChannelInputModel(label="DAPI")],
-        channels_to_exclude=None,
-        structure_props=["area"],
-        intensity_props=["intensity_total"],
+        target_label_names=["wf_2_labels", "wf_3_labels"],
         level="0",
         roi_table_name="FOV_ROI_table",
         append=False,
