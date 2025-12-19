d308a4c09386293fcf44c043c3154ddc7d2134b2
Flurin Sturzenegger <st.flurin@gmail.com>
2025-12-19 09:39:41 +0100

    add task_wrapper for add_type.py

--- src/zmb_fractal_tasks/add_type.py
+++ src/zmb_fractal_tasks/add_type.py
@@ -24,3 +24,9 @@ def add_type(
         "types": output_types,
     }
     return image_list_updates
+
+
+if __name__ == "__main__":
+    from fractal_task_tools.task_wrapper import run_fractal_task
+
+    run_fractal_task(task_function=add_type)
