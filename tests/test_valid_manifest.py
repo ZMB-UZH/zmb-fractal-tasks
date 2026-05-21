dd6b36415b980bc5238541cc4b6499b5c5ed29eb
Flurin Sturzenegger <st.flurin@gmail.com>
2026-05-21 11:48:17 +0200

    clean up fractal tooling tests no3

--- tests/test_valid_manifest.py
+++ tests/test_valid_manifest.py
@@ -1,14 +1,22 @@
+"""Tests for valid manifest."""
+
 import json
 from pathlib import Path
 
 import requests
 from jsonschema import validate
 
-import fractal_tasks_core
+import zmb_fractal_tasks
+
+PACKAGE_DIR = Path(zmb_fractal_tasks.__file__).parent
+MANIFEST_FILE = PACKAGE_DIR / "__FRACTAL_MANIFEST__.json"
+with MANIFEST_FILE.open("r") as f:
+    MANIFEST = json.load(f)
 
 
 def test_valid_manifest(tmp_path):
-    """
+    """Test if the manifest is valid according to the JSON schema.
+
     NOTE: to avoid adding a fractal-server dependency, we simply download the
     relevant file.
     """
@@ -19,15 +27,9 @@ def test_valid_manifest(tmp_path):
         "fractal_server/json_schemas/manifest_v2.json"
     )
     r = requests.get(url)
-    with (tmp_path / "manifest_schema_v2.json").open("wb") as f:
+    with (tmp_path / "manifest_schema.json").open("wb") as f:
         f.write(r.content)
-    with (tmp_path / "manifest_schema_v2.json").open("r") as f:
+    with (tmp_path / "manifest_schema.json").open("r") as f:
         manifest_schema = json.load(f)
 
-    # Load tasks-package manifest
-    module_dir = Path(fractal_tasks_core.__file__).parent
-    with (module_dir / "__FRACTAL_MANIFEST__.json").open("r") as fin:
-        manifest_dict = json.load(fin)
-
-    # Validate manifest against JSON Schema
-    validate(instance=manifest_dict, schema=manifest_schema)
+    validate(instance=MANIFEST, schema=manifest_schema)
